5e2ab9e3f08b735ce075c06129cff7b3b1fe5ea0
Ripbyrustavi02 <murathalitcan@gmail.com>
2017-06-24 06:17:35 +0300

    Question2Answer Anket Eklentisi Türkçe Paketi
    Anket Eklentisi Türkçe

---
+++ README.md.txt
@@ -0,0 +1,6 @@
+echo "# Question2Answer-Poll-Turkish-Pack" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Ripbyrustavi02/Question2Answer-Poll-Turkish-Pack.git
+git push -u origin master
---
+++ qa-poll-lang-default.php
@@ -0,0 +1,63 @@
+<?php
+	
+/*
+	Question2Answer (c) Gideon Greenspan #Türkçe Çeviri : Ripbyrustavi02
+
+	http://www.question2answer.org/ - http://ritopluluk.ga/
+
+	
+	Dosya: qa-plugin/example-page/qa-example-lang-default.php
+	Version: qa-include/qa-base.php
+	Description: Türkçeleştirilmiş Poll Plugin
+
+
+	This program is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
+	
+	Bu program ücretsiz yazılımdır.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	More about this license: http://www.question2answer.org/license.php
+*/
+
+	return array(
+		'permit_post_poll' => 'Anket oluştur',
+		'permit_vote_poll' => 'Anketi oyla',
+		'permit_close_poll' => 'Anketi kapat',
+		'permit_delete_poll' => 'Anketi sil',
+		'permit_delete_poll' => 'Anketi sil',
+		
+		'reopen_poll' => 'Yeniden aç',
+		'reopen_poll_hover' => 'Anketi yeniden aç',
+		'close_poll' => 'Kapat',
+		'close_poll_hover' => 'Anketi kapat',
+		'delete_poll' => 'Sil',
+		'delete_poll_hover' => 'Anketi sil',
+		
+		'poll_closed' => 'Anket kapatıldı',
+
+		'question_title' => '[Anket]',
+		'comments' => '[Anket]',
+		'checkbox_text' => 'Anket oluştur',
+		'multiple_text' => 'Çoklu oy kullanılsın mı?',
+		'vote_button' => 'Oyla',
+		'voted_button' => 'Oy kaldır',
+		'cannot_change' => 'Oy kaldırmaya izin verilmiyor.',
+		'already_voted' => 'Zaten oylamışsın',
+		'disabled_button' => 'En fazla 1 adet oy kullanabilirsin.',
+		'answers_text' => 'Şıklar:',
+		'page_title' => 'Anket',
+		'choice_count_error' => 'Çift şık girmelisin yoksa anket olmaz ki? :)',
+	);
+
+/*
+	http://Ritopluluk.ga/ tarafından Türkçeleştirilmiştir. İletişim : http://discord.murathalit.design
+	Omit PHP closing tag to help avoid accidental output
+	Unutmayın! Bu program ücretsizdir satılmaya çalışılırsa itibar etmeyiniz.
+*/
